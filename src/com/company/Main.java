e70d533e494e4872db53959d2b1b5286d504ecf8
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-04-16 09:25:26 +0300

    synchronized

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,28 +2,48 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-      System.out.println("Main thread started...");
-      new JThread("JThread").start();
-      System.out.println("Main thread finished...");
+  public static void main(String[] args) {
+    System.out.println("Main thread started...");
+    CommonResource commonResource = new CommonResource();
+    for (int i = 1; i < 6; i++) {
+      Thread t = new Thread(new CountThread(commonResource));
+      t.setName("Thread " + i);
+      t.start();
     }
+    System.out.println("Main thread finished...");
+  }
 }
 
-class JThread extends Thread {
+class CommonResource {
 
-  JThread(String name) {
-    super(name);
+  int x = 0;
+}
+
+class CountThread implements Runnable {
+
+ private final CommonResource res;
+
+  CountThread(CommonResource res) {
+    this.res = res;
   }
 
+
+  @Override
   public void run() {
-    System.out.printf("%s started... \n", Thread.currentThread().getName());
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-      System.out.println("Thread has been interrupted");
+    synchronized (res) {
+      res.x = 1;
+      System.out.printf("%s started... \n", Thread.currentThread().getName());
+      for (int i = 1; i < 5; i++) {
+        System.out.printf("%s %d \n", Thread.currentThread().getName(), res.x);
+        res.x++;
+        try {
+          Thread.sleep(5000);
+        } catch (InterruptedException e) {
+          System.out.println(e);
+        }
+      }
     }
-
     System.out.printf("%s finished... \n", Thread.currentThread().getName());
-
   }
 }
+
